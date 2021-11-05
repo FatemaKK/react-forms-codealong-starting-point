2a44be53353ea5c9dafea290f18e876ee14e4735
Fatema Kabir <fatema.kabir@pursuit.org>
2021-11-05 11:57:36 -0400

    working-form

--- src/App.js
+++ src/App.js
@@ -4,7 +4,37 @@ import emojify from "./emojify";
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      userInput: '',
+      feature: 'reverse',
+      result: '',
+    };
+  }
+
+  handleInputChange = (event) => {
+    // // change our state to reflect what the user has typed in
+    // this.setState({userInput: event.target.value})
+    // change our state to reverse what the user has typed in
+    if (this.state.feature === 'reverse') {
+        this.setState({
+          userInput: event.target.value,
+          result: event.target.value.split('').reverse().join(''),
+      })
+  } else if (this.state.feature === 'emojify') {
+        this.setState({
+           userInput: event.target.value,
+           result: emojify(event.target.value),
+      })
+    }
+  }
+
+  handleFeatureChange = (event) => {
+    this.setState({feature: event.target.value})
+  }
+
+  clearText = (event) => {
+    event.preventDefault();
+    this.setState({userInput: '', result: ''})
   }
 
   render() {
@@ -15,22 +45,26 @@ class App extends React.Component {
             id="user_input"
             name="user_input"
             type="text"
+            value={this.state.userInput}
+            onChange={this.handleInputChange}
           />
 
-          <select id="operation" name="operation">
+          <select 
+          id="operation" 
+          name="operation"
+          onChange={this.handleFeatureChange}
+          >
             <option value="reverse">reverse</option>
             <option value="emojify">emojify</option>
           </select>
 
-          <button>Clear Text</button>
-
-          <button type="submit">Check For Palindrome</button>
+          <button onClick={this.clearText}>Clear Text</button>
         </form>
 
         <section id="result">
-          <p>Current feature: reverse</p>
-          <p>You typed: </p>
-          <p>Result: </p>
+          <p>Current feature: {this.state.feature}</p>
+          <p>You typed: {this.state.userInput}</p>
+          <p>Result: {this.state.result}</p>
         </section>
       </main>
     );
